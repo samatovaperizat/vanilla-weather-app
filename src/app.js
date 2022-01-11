71114b28f4cf1b5850a1a6653bcc153be08c8610
Perizat <samatovaperizat@gmail.com>
2022-01-11 08:59:39 +0100

    added relevant icon to API

--- src/app.js
+++ src/app.js
@@ -25,17 +25,21 @@ function displayTemperature(response){
     let humidityElement=document.querySelector("#humidity");
     let windElement=document.querySelector("#humidity");
     let dateElement=document.querySelector("#date");
+    let iconElement=document.querySelector("#icon");
 temperatureElement.innerHTML=Math.round(response.data.main.temp);
 cityElement.innerHTML=response.data.name;
 descriptionElement.innerHTML=response.data.weather[0].description;
 humidityElement.innerHTML=response.data.main.humidity;
 wind.innerHTML=Math.round(response.data.wind.speed);
 dateElement.innerHTML=formatDate(response.data.dt * 1000);
-
+iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+
 let apiKey="c9ed1224b24aea572a7bf4ef19e27ccd";
-let apiUrl=`http://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city= "Bishkek";
+let apiUrl=`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
