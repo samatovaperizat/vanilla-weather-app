472acbca326673bc483d90fab5aa5fbf36bb49e6
Perizat <samatovaperizat@gmail.com>
2022-01-13 09:09:05 +0100

    added the search engine

--- src/app.js
+++ src/app.js
@@ -35,11 +35,22 @@ dateElement.innerHTML=formatDate(response.data.dt * 1000);
 iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-
-
+function search(city) {
 let apiKey="c9ed1224b24aea572a7bf4ef19e27ccd";
-let city= "Bishkek";
 let apiUrl=`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
-
 axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+   event.preventDefault(); 
+   let cityInputElement = document.querySelector("#city-input");
+   search(cityInputElement.value);
+}
+
+
+search("New York"
+)
+
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit)
