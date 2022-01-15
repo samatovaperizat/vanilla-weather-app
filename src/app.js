3a4e3a90c0f634347318590cd9f1a995b58c71e1
Perizat <samatovaperizat@gmail.com>
2022-01-15 16:38:09 +0100

    added the full days

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ function formatDay(timestamp){
 
     let date = new Date(timestamp*1000);
     let day = date.getDay();
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     return days[day];
 
 }
