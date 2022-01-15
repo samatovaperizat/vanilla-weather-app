840e234a83f1a29f72bd1df8d6c9b8e720684516
Perizat <samatovaperizat@gmail.com>
2022-01-15 16:34:35 +0100

    Display the forecast data using API

--- src/app.js
+++ src/app.js
@@ -15,32 +15,44 @@ function formatDate(timestamp){
 
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp){
+
+    let date = new Date(timestamp*1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    return days[day];
 
 }
+
 function displayForecast(response) {
-    console.log(response.data.daily);
+    let forecast = response.data.daily;
     let forecastElement = document.querySelector("#forecast");
   
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+    
     let forecastHTML = `<div class="row">`;
    
-   days.forEach(function(day){
+   forecast.forEach(function (forecastDay, index)
+   {
+       if (index < 6) {
+
+       
     forecastHTML = forecastHTML + `
        <div class="col-2">
-           <div class="weather-forecast-date">
-           ${day}
+           <div class="weather-forecast-date">${formatDay(forecastDay.dt)}
         </div>
         <div class="icon">
-           <img src="http://openweathermap.org/img/wn/50d@2x.png"
+           <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
             alt=""
         width="36"/> </div>
         <div class="weather-forecast-temperature">
-            <span class="weather-forecast-temperature-max">
-        18°
-    </span> <span class="weather-forecast-temperature-min">12°</span>
+            <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}°
+    </span> <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}°</span>
     </div>
     </div>
      `;
+    }
    })
       
      forecastHTML = forecastHTML + `</div>`;
@@ -81,7 +93,7 @@ dateElement.innerHTML=formatDate(response.data.dt * 1000);
 iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute("alt", response.data.weather[0].description);
 
-getForecast(response.data.coord)
+getForecast(response.data.coord);
 
 }
 function search(city) {
@@ -132,4 +144,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Göttingen");
-displayForecast();
