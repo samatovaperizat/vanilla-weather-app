cc15e0884993cf5415300d90ca77835f07f90dd6
Perizat <samatovaperizat@gmail.com>
2022-01-15 14:30:26 +0100

    added API

--- src/app.js
+++ src/app.js
@@ -17,10 +17,13 @@ function formatDate(timestamp){
     return `${day} ${hours}:${minutes}`;
 
 }
-function displayForecast(){
+function displayForecast(response) {
+    console.log(response.data.daily);
     let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = `<div class="row">`;
-   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+  
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+    let forecastHTML = `<div class="row">`;
+   
    days.forEach(function(day){
     forecastHTML = forecastHTML + `
        <div class="col-2">
@@ -45,6 +48,15 @@ function displayForecast(){
 
     
 }
+
+function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "c9ed1224b24aea572a7bf4ef19e27ccd";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response){
     let temperatureElement=document.querySelector("#temperature");
     let cityElement=document.querySelector("#city");
@@ -68,6 +80,9 @@ wind.innerHTML=Math.round(response.data.wind.speed);
 dateElement.innerHTML=formatDate(response.data.dt * 1000);
 iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute("alt", response.data.weather[0].description);
+
+getForecast(response.data.coord)
+
 }
 function search(city) {
 let apiKey="c9ed1224b24aea572a7bf4ef19e27ccd";
