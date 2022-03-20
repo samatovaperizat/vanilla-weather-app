c2e008523e18495d3143bc61f87bbc4ffe1c247e
Perizat <samatovaperizat@gmail.com>
2022-03-20 14:38:57 +0100

    the old keys

--- src/app.js
+++ src/app.js
@@ -63,7 +63,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
     console.log(coordinates);
-    let apiKey = "47a0e3e68fcc4da7aaf133441222003";
+    let apiKey = "6c6686f915ed3a277ddbc99a9f297cd7";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 axios.get(apiUrl).then(displayForecast);
@@ -97,7 +97,7 @@ getForecast(response.data.coord);
 
 }
 function search(city) {
-let apiKey="47a0e3e68fcc4da7aaf133441222003";
+let apiKey="6c6686f915ed3a277ddbc99a9f297cd7";
 let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
 }
